d47dde7abd25347931f3e73a2f691826f2721d1b
vanessaphtn <vanessa.apuhtin@gmail.com>
2023-05-05 17:05:33 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.oop_projekt2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.oop_projekt2 to javafx.fxml;
+    exports com.example.oop_projekt2;
+}
